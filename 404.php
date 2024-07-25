c0f4c346f06ac5f04812e2da42d5a8b97bcc54d3
ramyapixelflames <120080528+ramyapixelflames@users.noreply.github.com>
2024-07-25 17:39:01 +0530

    theme files

---
+++ 404.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+		  <div class="errorpage container">
+			<div class="page-header">
+				<h2 class="page-title">Not Found</h2>
+			</div>
+
+			<div class="page-wrapper">
+				<div class="page-content">
+					<p>It looks like nothing was found at this location</p>
+				</div>
+			</div>
+		  </div>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<header></header>
